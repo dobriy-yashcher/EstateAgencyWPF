21340cbb7df5a5f058d166471ca7b32615a6b4e2
dobriy-yashcher <golovinsergey17@gmail.com>
2022-11-28 10:39:17 +0300

    add WindowsApp, PagesApp.

--- EstateAgencyWPF/App.xaml.cs
+++ EstateAgencyWPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EstateAgencyWPF.ADOApp;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace EstateAgencyWPF
     /// </summary>
     public partial class App : Application
     {
+        public static EstateAgencyEntities Connection = new EstateAgencyEntities();
     }
 }
